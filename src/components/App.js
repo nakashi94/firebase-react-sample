eb03673c6f25592917a29d162add5d888b251755
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 18:48:15 +0900

    npm run build

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react"
-import { pushMessage } from "../firebase"
+import React, { useState, useEffect } from "react"
+import { messagesRef, pushMessage } from "../firebase"
 
 const App = () => {
-  const [name, setName] = useState('default')
-  const [text, setText] = useState('hello')
+  const [name, setName] = useState("default")
+  const [text, setText] = useState("text")
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(10)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val()
+        if (messages === null) return
+        const entries = Object.entries(messages)
+        const newMessages = entries.map((data) => {
+          const [key, message] = data
+          return { key, ...message }
+        })
+        setMessages(newMessages)
+      })
+  }, [])
 
   return (
     <>
+      {messages.map((message) => (
+        <div key={message.key}>
+          {message.name}:{message.text}
+        </div>
+      ))}
       <input
         type="text"
         value={name}
