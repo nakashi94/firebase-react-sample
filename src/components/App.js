f819cb66d63a63973d77078f5199c6d73f44d1d2
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-02 18:38:26 +0900

    edit App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import { pushMessage } from "../firebase"
 
 const App = () => {
-  return <div>hello</div>
+  const [name, setName] = useState('default')
+  const [text, setText] = useState('hello')
+
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName((name) => (name = e.target.value))}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText((text) => (text = e.target.value))}
+      />
+      <button onClick={() => pushMessage({ name: name, text: text })}>
+        {" "}
+        push{" "}
+      </button>
+    </>
+  )
 }
 
 export default App
